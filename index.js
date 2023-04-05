e8f9aca7fd7f8ac2c08e63b0cd73cd028c0c8c1d
Mutindi Hope <msndindi@outlook.com>
2023-04-06 00:54:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("go to the beach")); // Output: "This Saturday, I want to go to the beach!"
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  function wrapAdjective(highlight = "*") {
+    return function(adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
+  
